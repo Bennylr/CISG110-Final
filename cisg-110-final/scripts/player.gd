16c2d3245cfc499fc67ee5be10b7a73d790ae40d
Benjamin Luna-Rodriguez <benjaminlr1636@gmail.com>
2026-04-24 13:45:54 -0700

    added left and right kick

--- cisg-110-final/scripts/player.gd
+++ cisg-110-final/scripts/player.gd
@@ -4,6 +4,18 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+#add an exposed float variable for max time kick is enabled. (0.5)
+
+#add another float variable for kick timer
+
+@export var _kickRight: Node2D
+@export var _kickLeft: Node2D
+
+
+var _facingRight: bool = true
+
+func _enter_tree() -> void:
+	_disablekick()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,7 +23,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -21,5 +33,41 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	if direction > 0:
+		_facingRight = true
+	elif direction < 0:
+		_facingRight = false
+		
+		#check if timer is running (If the kick timer > 0)
+		#subtract delta from the timer
+		#if timer has ran out (if the timer < 0) 
+		#call disable kick
+		
+		if Input.is_action_just_pressed("ui_accept"):
+			_kick()
+			
 	move_and_slide()
+	
+	
+func _kick() -> void:
+	if _facingRight:
+		print("right kick")
+		_kickRight.process_mode = Node.PROCESS_MODE_INHERIT
+	else:
+		print("left kick")
+		_kickLeft.process_mode = Node.PROCESS_MODE_INHERIT
+	#set kick timer to max
+	
+	
+func _disablekick() -> void:
+	_kickRight.process_mode = Node.PROCESS_MODE_DISABLED
+	_kickLeft.process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func _on_kick_right_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print(body.name)
+
+
+func _on_kick_left_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print(body.name)
