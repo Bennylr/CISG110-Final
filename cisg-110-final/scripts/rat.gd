cc858f4b55f5b4843492f7e02affd3f132312f64
Benjamin Luna-Rodriguez <benjaminlr1636@gmail.com>
2026-05-04 13:56:42 -0700

    made rats move right and left

--- cisg-110-final/scripts/rat.gd
+++ cisg-110-final/scripts/rat.gd
@@ -9,18 +9,18 @@ func _ready() -> void:
 	contact_monitor = true
 	
 	
+	
 func _physics_process(delta: float) -> void:
 	var direction = 1 if _move_right else -1
 	linear_velocity = Vector2(direction * _speed, 0)
-	linear_velocity.x = -_speed
 	
 	move_and_collide(linear_velocity * delta)
 	
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
 	if body.name == "Player":
-		print("rat hit player and exploded")
-	if _explosion_VFX:
-		_explosion_VFX.global_position = global_position
+
+		if _explosion_VFX:
+			_explosion_VFX.global_position = global_position
 		_explosion_VFX.reparent(get_parent())
 		_explosion_VFX.emitting = true
 		
